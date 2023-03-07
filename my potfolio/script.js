1d1b481e25dcc477269337db69ca8e7934224216
Rakheeb Ahmad <101167115+RAKHEEBAHMAD@users.noreply.github.com>
2023-03-07 19:50:38 +0530

    Delete my potfolio directory

--- my potfolio/script.js
+++
@@ -1,9 +0,0 @@
-// Tuping Animation using typed.js
-var options={
-    strings: ["A Front-end Developer","A Programmer","An Engineer","Coding Enthusiasist"],
-    typeSpeed: 100,
-    backSpeed:100,
-    loop:true
-}
-var typed=new Typed('.typing',options)
-
